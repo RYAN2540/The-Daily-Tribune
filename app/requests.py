edbfa6e62e9c925a44a0efdbbad00ca04a6b2cad
Your Name <you@example.com>
2021-02-22 18:37:42 +0300

    add request function for article search from specific source

--- app/requests.py
+++ app/requests.py
@@ -114,6 +114,20 @@ def search_topic(query):
             search_topic_list = search_topic_response['articles']
             search_topic_results = process_news_results(search_topic_list)
 
+    return search_topic_results
+
+
+def search_from_source(query, source):
+    search_topic_url = 'https://newsapi.org/v2/everything?q={}&sortBy=relevancy,publishedAt&pageSize=30&source={}&apiKey={}'.format(query, source, api_key)
+    with urllib.request.urlopen(search_topic_url) as url:
+        search_topic_data = url.read()
+        search_topic_response = json.loads(search_topic_data)
+
+        search_topic_results = None
+
+        if search_topic_response['articles']:
+            search_topic_list = search_topic_response['articles']
+            search_topic_results = process_news_results(search_topic_list)
 
     return search_topic_results
 
