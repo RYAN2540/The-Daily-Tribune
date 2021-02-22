5ddadef1b4f5bc4ebac59db4e32fd6a02e3283f2
Your Name <you@example.com>
2021-02-22 17:09:36 +0300

    create news_list.html and render it from view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,11 @@
 from flask import render_template, request, redirect, url_for
 from . import main
-from ..requests import get_breaking_news,news_from_source,get_sources
+from ..requests import get_news,news_from_source,get_sources
 
 @main.route('/')
 @main.route('/home')
 def index():
-    breaking_news=get_breaking_news()
+    top_news=get_news()
     cnn=news_from_source("cnn")
     bbc=news_from_source("bbc-news")
     aljazeera=news_from_source("al-jazeera-english")
@@ -13,7 +13,7 @@ def index():
     politico=news_from_source("politico")
     cbs=news_from_source("cbs-news")
 
-    abc=news_from_source("abc-news")
+    newsweek=news_from_source("newsweek")
     fox=news_from_source("fox-news")
     time=news_from_source("time")
     nbc=news_from_source("nbc-news")
@@ -22,7 +22,7 @@ def index():
 
     sources=get_sources()
     title="The Daily Tribune"
-    return render_template('index.html', title=title, breaking_news=breaking_news, cnn=cnn, bbc=bbc, al=aljazeera,usa_today=usa_today, politico=politico, cbs=cbs, sources=sources, abc=abc, fox=fox, time=time, nbc=nbc, reuters=reuters, msnbc=msnbc)
+    return render_template('index.html', title=title, breaking_news=top_news, cnn=cnn, bbc=bbc, al=aljazeera,usa_today=usa_today, politico=politico, cbs=cbs, sources=sources, newsweek=newsweek, fox=fox, time=time, nbc=nbc, reuters=reuters, msnbc=msnbc)
 
 
 @main.route('/source/<id>')
@@ -30,4 +30,4 @@ def news_source(id):
     news_list=news_from_source(id)
     title=news_list[0].source_name
     sources=get_sources()
-    return render_template('news_source.html', title=title, news_list=news_list, sources=sources)
+    return render_template('news_list.html', title=title, news_list=news_list, sources=sources)
